08bd587b0ae6c62a217bd9f308b9129f78c670e6
Anniek van Dijk <anniek@animundo.nl>
2023-05-29 21:59:54 +0200

    more AD stuff

---
+++ src/app/models/profile.ts
@@ -0,0 +1,10 @@
+export interface Profile {
+    id: string;
+    givenName: string;
+    jobTitle: string;
+    mail: string;
+    mobilePhone: string;
+    surname: string;
+    userPrincipalName: string;
+    displayName: string;
+}
