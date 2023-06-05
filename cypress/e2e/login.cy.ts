52764525b027af0fa7c01fc6f056bbad5c79e47a
Anniek van Dijk <anniek@animundo.nl>
2023-06-05 13:10:36 +0200

    cypress login

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -1,12 +1,6 @@
 describe('Login', () => {
 
     it('should login', () => {
-        // cy.log('baseUrl: ', Cypress.env('BASE_URL'));
-        // cy.log('AZURE_CLIENT_ID: ', Cypress.env('AZURE_CLIENT_ID'));
-        // cy.visit(Cypress.env('BASE_URL'));
-        // cy.get('a[rel="login"]').click();
-        // cy.adlogin();
-
         cy.ADlogin()
         cy.visit('http://localhost:4200')
 
