7086fb1d9f6c383dbe352162a6b46fa964f54fdc
Anniek van Dijk <anniek.vandijk@getthere.nl>
2023-06-12 15:36:51 +0200

    login

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -1,7 +1,9 @@
 describe('Login', () => {
 
     it('should login with azure AD', () => {
-        cy.ADlogin()
+        const username = Cypress.env('AD_USERNAME')
+        const password = Cypress.env('AD_PASSWORD')
+        cy.ADlogin(username, password)
         cy.visit('http://localhost:4200')
     }
     );
