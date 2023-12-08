fc74d0425eace6aa53f815270981d8397dfa3d02
Anniek van Dijk <anniek@animundo.nl>
2023-12-08 20:09:17 +0100

    fix some bugs

--- src/app/features/home/home-sidebar/home-sidebar.component.ts
+++ src/app/features/home/home-sidebar/home-sidebar.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home-sidebar',
   standalone: true,
+  imports: [
+    RouterModule
+  ],
   templateUrl: './home-sidebar.component.html',
   styleUrls: []
 })
